56cac6a79a97428717843559d9ebde8feb781f50
choosak_rangmai <choosak.r@linecorp.com>
2021-05-20 12:16:23 +0700

    feat: add TH for i18n

---
+++ docs/js/picker.th-TH.js
@@ -0,0 +1,49 @@
+(function (global, factory) {
+  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+  typeof define === 'function' && define.amd ? define(factory) : factory(global.Picker);
+}(typeof self !== 'undefined' ? self : this, function (Picker) {
+  'use strict';
+
+  Picker.languages['pt-BR'] = {
+    months: [
+      'มกราคม',
+      'กุมภาพันธ์',
+      'มีนาคม',
+      'เมษายน',
+      'พฤษภาคม',
+      'มิถุนายน',
+      'กรกฎาคม',
+      'สิงหาคม',
+      'กันยายน',
+      'ตุลาคม',
+      'พฤศจิกายน',
+      'ธันวาคม'
+    ],
+    monthsShort: [
+      'ม.ค.',
+      'ก.พ.',
+      'มี.ค.',
+      'เม.ย.',
+      'พ.ค.',
+      'มิ.ย.',
+      'ก.ค.',
+      'ส.ค.',
+      'ก.ย.',
+      'ต.ค.',
+      'พ.ย.',
+      'ธ.ค.'
+    ],
+    text: {
+      title: 'เลือกวันที่และเวลา',
+      cancel: 'ยกเลิก',
+      confirm: 'ยืนยัน',
+      year: 'ปี',
+      month: 'เดือน',
+      day: 'วัน',
+      hour: 'ชั่วโมง',
+      minute: 'นาที',
+      second: 'วินาที',
+      millisecond: ''
+    }
+  };
+}));
